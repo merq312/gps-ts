c5a0da849df65b60021974819d67a3e8c6c04f7b
merq312 <merc312@gmail.com>
2022-01-29 21:54:08 -0500

    docs: add comments for distance calculation

--- src/utils.ts
+++ src/utils.ts
@@ -6,9 +6,6 @@ function degToRad(degrees: number) {
 }
 
 export function distanceBetween(coord1: gpsCoord, coord2: gpsCoord) {
-  // const radiusMin = 6357
-  // const radiusMax = 6378
-
   if (Math.abs(coord1.lat) > 90 || Math.abs(coord2.lat) > 90) {
     throw new Error('Latitude out of range')
   }
@@ -16,6 +13,8 @@ export function distanceBetween(coord1: gpsCoord, coord2: gpsCoord) {
     throw new Error('Longitude out of range')
   }
 
+  // const radiusMin = 6357
+  // const radiusMax = 6378
   const radiusAvg = 6371
 
   const deltaLatDiv2 = degToRad(coord2.lat - coord1.lat) / 2
@@ -24,6 +23,8 @@ export function distanceBetween(coord1: gpsCoord, coord2: gpsCoord) {
   const lat1 = degToRad(coord1.lat)
   const lat2 = degToRad(coord2.lat)
 
+  // a => square of half the chord length between the points
+  // c => angular distance in radians
   const a =
     Math.sin(deltaLatDiv2) * Math.sin(deltaLatDiv2) +
     Math.sin(deltaLonDiv2) *
@@ -34,3 +35,5 @@ export function distanceBetween(coord1: gpsCoord, coord2: gpsCoord) {
 
   return radiusAvg * c
 }
+
+// http://www.movable-type.co.uk/scripts/latlong.html
